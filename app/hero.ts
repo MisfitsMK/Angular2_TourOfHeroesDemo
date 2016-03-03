4ed7cea245d5ec4ca4ed35ba93e4329cbb5766a0
MisfitsMK <patchizzle@gmail.com>
2016-03-02 23:09:00 -0800

    initial commit
    Adding base files and NPM package.json files. Will need to run 'npm
    install' before everything works. Then run 'npm start' to launch the
    local server and run the app.

---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+﻿export interface Hero {
+    id: number;
+    name: string;
+    power: string;
+    
+    clickBonus: number;
+}
---
+++ app/mock-heroes.js
@@ -0,0 +1,135 @@
+System.register([], function(exports_1) {
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [{
+                    "id": 1,
+                    "name": "Terry Marshall",
+                    "power": "NICE Call Recording",
+                    "clickBonus": 3
+                }, {
+                    "id": 2,
+                    "name": "Andrew Sims",
+                    "power": "Sun Certified Java Programmer",
+                    "clickBonus": 13
+                }, {
+                    "id": 3,
+                    "name": "Theresa Baker",
+                    "power": "Ulead VideoStudio",
+                    "clickBonus": 15
+                }, {
+                    "id": 4,
+                    "name": "Michael Robinson",
+                    "power": "TCAS",
+                    "clickBonus": 23
+                }, {
+                    "id": 5,
+                    "name": "Walter Henry",
+                    "power": "PMCS",
+                    "clickBonus": 19
+                }, {
+                    "id": 6,
+                    "name": "Christopher Perry",
+                    "power": "VLSI",
+                    "clickBonus": 11
+                }, {
+                    "id": 7,
+                    "name": "Joseph Graham",
+                    "power": "CMS",
+                    "clickBonus": 18
+                }, {
+                    "id": 8,
+                    "name": "Ann Kelley",
+                    "power": "Cardiology",
+                    "clickBonus": 16
+                }, {
+                    "id": 9,
+                    "name": "Gary Fowler",
+                    "power": "Aeroelasticity",
+                    "clickBonus": 16
+                }, {
+                    "id": 10,
+                    "name": "Steven Holmes",
+                    "power": "Jenkins",
+                    "clickBonus": 25
+                }, {
+                    "id": 11,
+                    "name": "Anthony Reid",
+                    "power": "ZPL",
+                    "clickBonus": 20
+                }, {
+                    "id": 12,
+                    "name": "Elizabeth Baker",
+                    "power": "Rheology",
+                    "clickBonus": 6
+                }, {
+                    "id": 13,
+                    "name": "Lori Hill",
+                    "power": "DxDesigner",
+                    "clickBonus": 13
+                }, {
+                    "id": 14,
+                    "name": "Michael George",
+                    "power": "Korean",
+                    "clickBonus": 4
+                }, {
+                    "id": 15,
+                    "name": "Timothy Cruz",
+                    "power": "MS-DOS",
+                    "clickBonus": 15
+                }, {
+                    "id": 16,
+                    "name": "Ruth Matthews",
+                    "power": "Indoor Air Quality",
+                    "clickBonus": 2
+                }, {
+                    "id": 17,
+                    "name": "Patrick Williamson",
+                    "power": "LCS",
+                    "clickBonus": 8
+                }, {
+                    "id": 18,
+                    "name": "Ashley Banks",
+                    "power": "Urinalysis",
+                    "clickBonus": 9
+                }, {
+                    "id": 19,
+                    "name": "Virginia Washington",
+                    "power": "Agilent ADS",
+                    "clickBonus": 19
+                }, {
+                    "id": 20,
+                    "name": "Scott Elliott",
+                    "power": "Protein Purification",
+                    "clickBonus": 12
+                }, {
+                    "id": 21,
+                    "name": "Ruth Gordon",
+                    "power": "OOP",
+                    "clickBonus": 6
+                }, {
+                    "id": 22,
+                    "name": "Howard Harris",
+                    "power": "eHRPD",
+                    "clickBonus": 5
+                }, {
+                    "id": 23,
+                    "name": "Diana Jackson",
+                    "power": "Investment Properties",
+                    "clickBonus": 7
+                }, {
+                    "id": 24,
+                    "name": "Kenneth Jacobs",
+                    "power": "MVC Architecture",
+                    "clickBonus": 12
+                }, {
+                    "id": 25,
+                    "name": "Martha Welch",
+                    "power": "MMI",
+                    "clickBonus": 1
+                }]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
